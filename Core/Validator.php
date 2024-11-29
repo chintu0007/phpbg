550c661c7bd17e3cdf8fc401f82b866f8bfeffa8
Gaurang Parekh <gaurang9rising@gmail.com>
2024-11-29 17:47:01 +0530

    testing approaches, term and considerations

--- Core/Validator.php
+++ Core/Validator.php
@@ -1,15 +1,23 @@
-<?php 
-namespace Core;
-class Validator {
+<?php
 
-    public static function string ($value, $min = 1, $max = INF) {
+namespace Core;
+
+class Validator
+{
+    public static function string($value, $min = 1, $max = INF)
+    {
         $value = trim($value);
 
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
-    public static function email($value)
+    public static function email(string $value): bool
     {
         return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function greaterThan(int $value, int $greaterThan): bool
+    {
+        return $value > $greaterThan;
     }
 }
